2bad7fae963e8070f95ca2f75510f4989ee47b7b
yikang <1131487340@qq.com>
2020-03-21 16:02:36 +0800

    make it a api

--- dataset_split.py
+++ dataset_split.py
@@ -1,42 +1,84 @@
 import random
 
-PATH = "/home/yikang/Documents/dataset/ml-20m/"
-STORED_PATH = PATH + "subset/"
-mp = dict()
-test_line = set()
-with open(STORED_PATH + 'u_i_t_clustered.dat') as rf:
-    lineCnt = 0
-    while True:
-        line = rf.readline()
-        if not line:
-            break
-        lineCnt += 1
-        cap = line.strip().split('\t')
-        user = int(cap[0])
-        item = int(cap[1])
-        tag = int(cap[2])
-        if user not in mp:
-            mp[user] = dict()
-        if tag not in mp[user]:
-            mp[user][tag] = list()
-        mp[user][tag].append(lineCnt)
-for key, value in mp.items():
-    index = random.randint(0, len(value) - 1)
-    for v in value.values():
-        if index == 0:
-            test_line.update(v)
-            break
-        index -= 1
-with open(STORED_PATH + 'u_i_t_test.dat', 'w') as wf1:
-    with open(STORED_PATH + 'u_i_t_train.dat', 'w') as wf2:
-        with open(STORED_PATH + 'u_i_t_clustered.dat') as rf:
-            lineCnt = 0
-            while True:
-                line = rf.readline()
-                if not line:
-                    break
-                lineCnt += 1
-                if lineCnt in test_line:
-                    wf1.write(line)
-                else:
-                    wf2.write(line)
+
+# PATH = "/home/yikang/Documents/dataset/ml-20m/"
+# STORED_PATH = PATH + "subset/"
+# mp = dict()
+# test_line = set()
+# with open(STORED_PATH + 'u_i_t_clustered.dat') as rf:
+#     lineCnt = 0
+#     while True:
+#         line = rf.readline()
+#         if not line:
+#             break
+#         lineCnt += 1
+#         cap = line.strip().split('\t')
+#         user = int(cap[0])
+#         item = int(cap[1])
+#         tag = int(cap[2])
+#         if user not in mp:
+#             mp[user] = dict()
+#         if tag not in mp[user]:
+#             mp[user][tag] = list()
+#         mp[user][tag].append(lineCnt)
+# for key, value in mp.items():
+#     index = random.randint(0, len(value) - 1)
+#     for v in value.values():
+#         if index == 0:
+#             test_line.update(v)
+#             break
+#         index -= 1
+# with open(STORED_PATH + 'u_i_t_test.dat', 'w') as wf1:
+#     with open(STORED_PATH + 'u_i_t_train.dat', 'w') as wf2:
+#         with open(STORED_PATH + 'u_i_t_clustered.dat') as rf:
+#             lineCnt = 0
+#             while True:
+#                 line = rf.readline()
+#                 if not line:
+#                     break
+#                 lineCnt += 1
+#                 if lineCnt in test_line:
+#                     wf1.write(line)
+#                 else:
+#                     wf2.write(line)
+
+
+def go(file_in, test_out, train_out):
+    mp = dict()
+    test_line = set()
+    with open(file_in) as rf:
+        lineCnt = 0
+        while True:
+            line = rf.readline()
+            if not line:
+                break
+            lineCnt += 1
+            cap = line.strip().split('\t')
+            user = int(cap[0])
+            item = int(cap[1])
+            tag = int(cap[2])
+            if user not in mp:
+                mp[user] = dict()
+            if tag not in mp[user]:
+                mp[user][tag] = list()
+            mp[user][tag].append(lineCnt)
+    for key, value in mp.items():
+        index = random.randint(0, len(value) - 1)
+        for v in value.values():
+            if index == 0:
+                test_line.update(v)
+                break
+            index -= 1
+    with open(test_out, 'w') as wf1:
+        with open(train_out, 'w') as wf2:
+            with open(file_in) as rf:
+                lineCnt = 0
+                while True:
+                    line = rf.readline()
+                    if not line:
+                        break
+                    lineCnt += 1
+                    if lineCnt in test_line:
+                        wf1.write(line)
+                    else:
+                        wf2.write(line)
