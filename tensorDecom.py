c2654f78ac05300fb9b5d15b5722178382141555
yikang <1131487340@qq.com>
2020-03-21 10:57:30 +0800

    modify some parameters of alsTucker

--- tensorDecom.py
+++ tensorDecom.py
@@ -8,9 +8,9 @@ PATH = "/home/yikang/Documents/dataset/ml-20m/"
 STORED_PATH = PATH + "subset/"
 U_LEN = subsetGen.get_len(0)
 I_LEN = subsetGen.get_len(1)
-T_LEN = 7
+T_LEN = 10
 x = np.zeros([U_LEN, I_LEN, T_LEN])
-with open(STORED_PATH + 'u_i_t_clustered.dat') as rf:
+with open(STORED_PATH + 'u_i_t_train.dat') as rf:
     while True:
         line = rf.readline()
         if not line:
@@ -21,16 +21,15 @@ with open(STORED_PATH + 'u_i_t_clustered.dat') as rf:
         tag = int(cap[2])
         x[user - 1, item - 1, tag - 1] = 1.0
 
-core, factors = tucker(x, [U_LEN / 2, I_LEN / 2, T_LEN])
+core, factors = tucker(x, [int(U_LEN * 0.19), int(I_LEN * 0.2), int(T_LEN)])
 mx = tucker_to_tensor((core, factors))
-print("reconstructed tensor:\n%s" % str(mx))
 # read test data
-mp_item = dict()  # user-item
-mp_set = dict()  # user-tag set
+mp_tag = dict()  # user-tag
+mp_set = dict()  # user-item set
 # evaluation
 Prec = list()
 Recall = list()
-for i in range(0, 10):
+for i in range(0, T_LEN):
     Prec.append(0.0)
     Recall.append(0.0)
 with open(STORED_PATH + 'u_i_t_test.dat') as rf:
@@ -42,18 +41,18 @@ with open(STORED_PATH + 'u_i_t_test.dat') as rf:
         user = int(cap[0]) - 1
         item = int(cap[1]) - 1
         tag = int(cap[2]) - 1
-        if user not in mp_item:
-            mp_item[user] = item
+        if user not in mp_tag:
+            mp_tag[user] = tag
         if user not in mp_set:
             mp_set[user] = set()
-        mp_set[user].add(tag)
+        mp_set[user].add(item)
 
-for top in range(1, 11):
-    for user, item in mp_item.items():
-        u_i_list = mx[user, item, :].tolist()
+for top in range(1, T_LEN + 1):
+    for user, tag in mp_tag.items():
+        u_t_list = mx[user, :, tag].tolist()
         tmp_list = list()  # [[value, index],...]
         cnt = 0
-        for i in u_i_list:
+        for i in u_t_list:
             tmp_list.append([i, cnt])
             cnt += 1
         sorted_list = sorted(tmp_list, key=operator.itemgetter(0), reverse=True)
@@ -65,3 +64,5 @@ for top in range(1, 11):
         Recall[top - 1] += float(len(z) / len(mp_set[user]))
     Prec[top - 1] /= U_LEN
     Recall[top - 1] /= U_LEN
+print(Prec)
+print(Recall)
