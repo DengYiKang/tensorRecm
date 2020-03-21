1f8da2119f6d9068bfe107d1380d9abf3efe39f1
yikang <1131487340@qq.com>
2020-03-21 16:34:42 +0800

    only use alsTucker

---
+++ control group/tensor_reduction.py
@@ -0,0 +1,13 @@
+from tensorly.decomposition import tucker
+from tensorly import tucker_to_tensor
+import numpy as np
+import subsetGen
+import dataset_split
+import tensorDecom
+
+# only use alsTucker
+
+PATH = "/home/yikang/Documents/dataset/ml-20m/"
+STORED_PATH = PATH + "subset/"
+dataset_split.go(STORED_PATH + 'u_i_t_fin.dat', STORED_PATH + 'u_i_t_test.dat', STORED_PATH + 'u_i_t_train.dat')
+tensorDecom.go(subsetGen.get_len(0), subsetGen.get_len(1), subsetGen.get_len(2))
